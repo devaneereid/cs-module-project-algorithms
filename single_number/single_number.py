8d7de3c7a9e1b38d62d13961b2b113c62a3134ff
Devanee <devaneereid@gmail.com>
2020-07-22 14:38:28 -0600

    finished single_number function/test passes

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,18 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
 
-    pass
+    result = []
 
+    for i in arr:
+        if i not in result:
+            result.append(i)
+            
+        else:
+            result.remove(i)
+
+    return result[0]
+    
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
